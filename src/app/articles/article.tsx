f2efa2915712dc05e86818b4e2e8274ed78bb0cb
cesaruseche18 <cesar@vaangroup.com>
2025-03-26 16:13:22 -0400

    Related articles

---
+++ src/app/articles/article.tsx
@@ -0,0 +1,38 @@
+import ArticleAction from "@/components/article-action";
+import ArticleDetails from "@/components/article-details";
+import RelatedArticles from "@/components/related-articles";
+import ArticleClient from "./[slug]/article-client";
+import styles from "./[slug]/style.module.scss";
+
+
+type Props = {
+  image: string;
+  title: string;
+  description: string;
+  date: string;
+  content: string;
+  tag: string;
+  author: string;
+  duration: string;
+  category: string;
+};
+
+export default function Article(props: Props) {
+  return (
+    <div className={styles.article}>
+      <ArticleAction category={props.category} />
+
+      <div className={styles.container}>
+        <div className={styles.grid}>
+          <aside className={styles.article__aside}>
+            <div className={styles.article__sidebar}>
+              <ArticleDetails image={props.image} date={props.date} author={props.author} duration={props.duration} />
+            </div>
+          </aside>
+          <ArticleClient {...props} />
+        </div>
+      </div>
+      <RelatedArticles />
+    </div>
+  );
+}
