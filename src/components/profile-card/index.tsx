a94cba4fcc7f9a1da5f7e4c3470c1c5dbf71d02e
cesaruseche18 <cesar@vaangroup.com>
2025-03-31 16:36:52 -0400

    Fix eslint

--- src/components/profile-card/index.tsx
+++ src/components/profile-card/index.tsx
@@ -6,7 +6,6 @@ import IconGithub from "../../components/icons/icon-github";
 import IconLinkedIn from "../../components/icons/icon-linkedin";
 import styles from "./style.module.scss";
 
-
 export default function ProfileCard() {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -46,8 +45,8 @@ export default function ProfileCard() {
               Cesar <br /> Useche
             </h1>
             <p className={styles.intro__text}>
-              Hey there! I'm Cesar, a software engineer deeply passionate about technology.
-              I'm constantly sharpening my skills and currently pursuing a Master's degree in Computer Science,
+              Hey there! I&apos;m Cesar, a software engineer deeply passionate about technology.
+              I&apos;m constantly sharpening my skills and currently pursuing a Master&apos;s degree in Computer Science,
               diving into the exciting fields of Machine Learning and AI. Outside of tech, I enjoy exploring other
               interests like producing music, hitting the tennis court, etc.
             </p>
@@ -55,13 +54,13 @@ export default function ProfileCard() {
               Professionally, I focus on building robust and scalable web applications.
               With expertise in React, Next.js, and TypeScript, I enjoy crafting efficient architectures and seamless,
               visually engaging user experiences. I thrive on tackling complex challenges and optimizing for performance
-              and usability.  
+              and usability.
             </p>
             <p className={styles.main__text}>
               My background includes leading full-stack development projects, utilizing technologies like GraphQL, Redux/Xstate, and Node.js.
-              I also have experience integrating complex animations with GSAP  and leveraging tools like Docker for streamlined development.
+              I also have experience integrating complex animations with GSAP and leveraging tools like Docker for streamlined development.
               Whether working on sophisticated applications or collaborating with UI/UX designers, my goal is always to deliver maintainable,
-              high-quality code.  
+              high-quality code.
             </p>
           </div>
         </div>
